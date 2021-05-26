d393f1299701dae0e7290daf3b539fcd8b47d7ca
Max16 <max16@naver.com>
2021-05-26 16:53:28 +0900

    #6.3 Upload Photo part Two

--- photos/uploadPhoto/uploadPhoto.resolvers.js
+++ photos/uploadPhoto/uploadPhoto.resolvers.js
@@ -1,3 +1,4 @@
+import client from "../../client";
 import { protectedResolver } from "../../users/users.utils";
 
 export default {
@@ -6,7 +7,27 @@ export default {
       async (_, { file, caption }, { loggedInUser }) => {
         if (caption) {
           // parse caption
+          const hashtags = caption.match(/#[ㄱ-ㅎ|ㅏ-ㅣ|가-힣|\w]+/g);
+          console.log(hashtags);
           // get or create Hasntags
+          client.photo.create({
+            data: {
+              file,
+              caption,
+              hashtag: {
+                connectOrCreate: [
+                  {
+                    where: {
+                      hashtag: "#food",
+                    },
+                    create: {
+                      hashtag: "#food",
+                    },
+                  },
+                ],
+              },
+            },
+          });
         }
         // save the photo WITH the parsed Hashtags
         // add the photo to the Hashtags
