6cc0e05741085c263b9b9205e5c732b7a66720a2
Max16 <max16@naver.com>
2021-05-26 10:36:39 +0900

    #4.24 Followers pagination part Two

--- users/seeFollers/seeFollers.resolvers.js
+++ users/seeFollers/seeFollers.resolvers.js
@@ -3,15 +3,31 @@ import client from "../../client";
 export default {
   Query: {
     seeFollowers: async (_, { username, page }) => {
+      const ok = await client.user.findUnique({
+        where: { username },
+        select: { id: true }, // id 만 확인해도 충분함
+      });
+      if (!ok) {
+        return {
+          ok: false,
+          error: "User not found.",
+        };
+      }
       const followers = await client.user
         .findUnique({ where: { username } })
         .followers({
           take: 5,
-          skip: (page - 1) * 5,
+          skip: (page - 1) * 5, // 5개씩 page 수에 따라 skip
         });
+      const totalFollowers = await client.user.count({
+        where: {
+          following: { some: { username } }, // following list 에 username 이 있는 user 만 count
+        },
+      });
       return {
         ok: true,
         followers,
+        totalPages: Math.ceil(totalFollowers / 5),
       };
     },
   },
