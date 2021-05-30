9ff3adc6f78c78444c0efaf0f9163c30ca17003c
Max16 <max16@naver.com>
2021-05-30 15:19:50 +0900

    #7.6 Super Test

--- messages/messages.resolvers.js
+++ messages/messages.resolvers.js
@@ -29,4 +29,7 @@ export default {
       });
     },
   },
+  Message: {
+    user: ({ id }) => client.message.findUnique({ where: { id } }).user(),
+  },
 };
--- messages/sendMessage/sendMessage.resolvers.js
+++ messages/sendMessage/sendMessage.resolvers.js
@@ -2,73 +2,74 @@ import client from "../../client";
 import { protectedResolver } from "../../users/users.utils";
 
 export default {
-  Mutation: protectedResolver(
-    async (_, { payload, roomId, userId }, { loggedInUser }) => {
-      let room = null;
-
-      if (userId) {
-        const user = await client.user.findUnique({
-          where: {
-            id: userId,
-          },
-          select: {
-            id: true,
-          },
-        });
-        if (!user) {
-          return {
-            ok: false,
-            error: "This user does not exist.",
-          };
+  Mutation: {
+    sendMessage: protectedResolver(
+      async (_, { payload, roomId, userId }, { loggedInUser }) => {
+        let room = null;
+        if (userId) {
+          const user = await client.user.findUnique({
+            where: {
+              id: userId,
+            },
+            select: {
+              id: true,
+            },
+          });
+          if (!user) {
+            return {
+              ok: false,
+              error: "This user does not exist.",
+            };
+          }
+          room = await client.room.create({
+            data: {
+              users: {
+                connect: [
+                  {
+                    id: userId, // 우리가 찾는 user
+                  },
+                  {
+                    id: loggedInUser.id, // login 한 user
+                  },
+                ],
+              },
+            }, // newRoom 생성
+          });
+        } else if (roomId) {
+          room = await client.room.findUnique({
+            where: {
+              id: roomId,
+            },
+            select: {
+              id: true,
+            },
+          });
+          if (!room) {
+            return {
+              ok: false,
+              error: "Room not found.",
+            };
+          }
         }
-        room = await client.room.create({
+        await client.message.create({
           data: {
-            users: {
-              connect: [
-                {
-                  id: userId, // 우리가 찾는 user
-                },
-                {
-                  id: loggedInUser.id, // login 한 user
-                },
-              ],
+            payload, // sending message
+            room: {
+              connect: {
+                id: room.id, // 방금 만든 room
+              },
             },
-          }, // newRoom 생성
-        });
-      } else if (roomId) {
-        room = await client.room.findUnique({
-          where: {
-            id: roomId,
-          },
-          select: {
-            id: true,
+            user: {
+              connect: {
+                id: loggedInUser.id, // login 한 user
+              },
+            },
           },
         });
-        if (!room) {
-          return {
-            ok: false,
-            error: "Room not found.",
-          };
-        }
+        return {
+          ok: true,
+        };
       }
-      await client.message.create({
-        data: {
-          payload, // sending message
-          room: {
-            connect: {
-              id: room.id, // 방금 만든 room
-            },
-          },
-          user: {
-            connect: {
-              id: loggedInUser.id, // login 한 user
-            },
-          },
-        },
-      });
-      return {
-        ok: true,
-      };
-    }
-  ),
+    ),
+  },
 };
