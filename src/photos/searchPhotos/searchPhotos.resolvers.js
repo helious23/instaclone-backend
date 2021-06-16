2997afcfb7fae51ac138a0669ffffa7459f35515
Max16 <max16@naver.com>
2021-06-16 13:07:30 +0900

    searchPhotos delete pasination

--- src/photos/searchPhotos/searchPhotos.resolvers.js
+++ src/photos/searchPhotos/searchPhotos.resolvers.js
@@ -2,15 +2,13 @@ import client from "../../client";
 
 export default {
   Query: {
-    searchPhotos: (_, { keyword, page }) => {
+    searchPhotos: (_, { keyword }) => {
       const photos = client.photo.findMany({
         where: {
           caption: {
             contains: keyword,
           },
         },
-        take: 5,
-        skip: page,
       });
 
       return {
